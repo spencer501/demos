3f7f7005732987aaef6b1922e2717ca9dbfde7ee
Spencer <spencer.p.816@gmail.com>
2017-02-13 11:15:23 -0800

    Complete in class work

--- nhanes-analysis/activity.R
+++ nhanes-analysis/activity.R
@@ -1,7 +1,7 @@
 # Calculate survey weighted estimates
 
 # Set up
-setwd('~/Documents/info-498c/demos/nhanes-analysis/')
+setwd('~/Code/info498c/demos/nhanes-analysis/')
 # library(foreign)
 library(survey)
 library(dplyr)
@@ -17,22 +17,24 @@ alcohol <- sasxport.get('./data/ALQ_H.XPT')
 nhanes <- left_join(alcohol, demographics, by='seqn')
 
 # Take the sum of the weighting column `wtint2yr` - what is this number?
-
+sum(nhanes['wtint2yr']) # This represents the population size
 
 # Create a survey design that indicates the id, strata, and weights
-
+nhanes.survey <- svydesign(id = ~nhanes$seqn,
+                           strata = ~nhanes$sdmvstra,
+                           weight = ~nhanes$wtint2yr)
 
 # Using the codebook, find the question that asks about 12+ drinks in the last year
-
+# alq101
 
 # Using the `table` function get a table of 12+ drinks in the last year responses
-
+table(nhanes$alq101)
 
 # Using the `prop.table` function, get the proportions of each response
-
+prop.table(table(nhanes$alq101))
 
 # Using the `svytable` function, compute the survey weighted responses to the same question
-
+svytable(~nhanes$alq101, design=nhanes.survey)
 
 # Using `prop.table` in conjunction with `svytable`, compute the survey weighted proportions
-
+prop.table(svytable(~nhanes$alq101, design=nhanes.survey))
